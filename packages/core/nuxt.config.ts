f5ebd19028cee86d597e8b9ed97f61927e4e6e4d
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-09-13 20:59:11 +0200

    fix: resolve relative import

--- packages/core/nuxt.config.ts
+++ packages/core/nuxt.config.ts
@@ -1,13 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
+
+// needed for layer relative import (https://nuxt.com/docs/guide/going-further/layers#relative-paths-and-aliases)
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/fonts'],
-  css: ["~/assets/base.css"],
+  modules: ["@nuxt/ui", "@nuxt/fonts"],
+  css: [join(currentDir, "./assets/base.css")],
   fonts: {
-    families: [
-      { name: 'Inter', provider: 'google' },
-    ]
+    families: [{ name: "Inter", provider: "google" }],
   },
   colorMode: {
     preference: "light",
