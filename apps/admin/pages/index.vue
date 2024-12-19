7ddcc085c2502ad8d5e0074567b3b248d149224c
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-12-19 20:19:10 +0100

    feat: add homepage

--- apps/admin/pages/index.vue
+++ apps/admin/pages/index.vue
@@ -3,45 +3,6 @@ const title = ref<string>("Home")
 useHead({
   title: () => title.value,
 })
-
-const options = [
-  {
-    label: "Live",
-    color: "bg-red-100 text-red-500",
-  },
-  {
-    label: "Dashboard",
-    color: "bg-orange-100 text-orange-500",
-  },
-  {
-    label: "Turniere",
-    color: "bg-yellow-100 text-yellow-500",
-  },
-  {
-    label: "Anmeldung",
-    color: "bg-lime-100 text-lime-500",
-  },
-  {
-    label: "Aktuelles",
-    color: "bg-green-100 text-green-500",
-  },
-  {
-    label: "Galerie",
-    color: "bg-purple-100 text-purple-500",
-  },
-  {
-    label: "Aktivität",
-    color: "bg-cyan-100 text-cyan-500",
-  },
-  {
-    label: "Verwaltung",
-    color: "bg-blue-100 text-blue-500",
-  },
-  {
-    label: "Einstellungen",
-    color: "bg-rose-100 text-rose-500",
-  },
-]
 </script>
 
 <template>
@@ -54,31 +15,50 @@ const options = [
         <h1 class="font-serif text-3xl tracking-tight">
           Willkommen bei tourney.
         </h1>
-        <div class="flex h-full w-full flex-col space-y-3">
-          <h2 class="mt-6 font-mono tracking-tight">QUICKSTART</h2>
-          <div class="flex grow gap-12">
-            <div class="grow space-y-2">
-              <div class="rounded-md border border-gray-200 bg-gray-50 p-2">
-                GitHub
-              </div>
-              <div class="rounded-md border border-gray-200 bg-gray-50 p-2">
-                Schülerapp
-              </div>
-              <div class="rounded-md border border-gray-200 bg-gray-50 p-2">
-                Dokumentation
-              </div>
-            </div>
-            <div class="grid w-full shrink grid-cols-3 gap-2">
-              <div
-                v-for="option in options"
-                :key="option.label"
-                :class="[option.color]"
-                class="flex h-28 w-full flex-col items-center justify-center gap-1 rounded-lg font-medium"
-              >
-                <p>{{ option.label }}</p>
-              </div>
-            </div>
+        <div class="mt-6 flex h-full w-full flex-col gap-3">
+          <div class="flex h-1/2 gap-3">
+            <!-- TODO: add link -->
+            <NuxtLink
+              rel="noopener"
+              target="_blank"
+              to=""
+              class="hover:border-primary-500 dark:hover:border-primary-500 flex h-full w-2/3 flex-col justify-between rounded-lg border bg-gray-50 p-8 font-serif text-xl tracking-tight dark:border-gray-700 dark:bg-gray-900"
+            >
+              <p>Dokumentation</p>
+              <UIcon
+                name="i-heroicons-arrow-up-right"
+                class="self-end text-gray-500"
+                size="24"
+              />
+            </NuxtLink>
+            <NuxtLink
+              rel="noopener"
+              target="_blank"
+              to="https://github.com/Dino-Kupinic/tourney"
+              class="hover:border-primary-500 dark:hover:border-primary-500 flex h-full w-1/3 flex-col justify-between rounded-lg border bg-gray-50 p-8 font-serif text-xl tracking-tight dark:border-gray-700 dark:bg-gray-900"
+            >
+              <p>GitHub</p>
+              <UIcon
+                name="i-heroicons-arrow-up-right"
+                class="self-end text-gray-500"
+                size="24"
+              />
+            </NuxtLink>
           </div>
+          <!-- TODO: add link -->
+          <NuxtLink
+            rel="noopener"
+            target="_blank"
+            to=""
+            class="hover:border-primary-500 dark:hover:border-primary-500 flex h-1/3 w-full flex-col justify-between rounded-lg border bg-gray-50 p-8 font-serif text-xl tracking-tight dark:border-gray-700 dark:bg-gray-900"
+          >
+            <p>Schüler-App</p>
+            <UIcon
+              name="i-heroicons-arrow-up-right"
+              class="self-end text-gray-500"
+              size="24"
+            />
+          </NuxtLink>
         </div>
       </div>
     </div>
