3cfb10fe0f49aea9a0f6288df76330fa9b168785
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-12-20 09:35:41 +0100

    feat: minor changes

--- apps/viewer/components/layout/TheDesktopNavigation.vue
+++ apps/viewer/components/layout/TheDesktopNavigation.vue
@@ -4,9 +4,12 @@ const links = getNavLinks()
 
 <template>
   <div
-    class="m-auto flex w-[1024px] items-center border border-gray-200 bg-gray-50 px-2 2xl:w-[1366px] dark:border-gray-800 dark:bg-gray-900"
+    class="m-auto flex w-[1024px] items-center justify-between border border-gray-200 bg-gray-50 px-2 px-6 2xl:w-[1366px] dark:border-gray-800 dark:bg-gray-900"
   >
-    <UHorizontalNavigation :links="links" />
+    <Logo class="h-9 w-9" />
+    <div>
+      <UHorizontalNavigation :links="links" />
+    </div>
     <SettingsButton size="sm" />
   </div>
 </template>
