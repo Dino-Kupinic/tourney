d3695ebfba296dbdbb559b8a4ab221cabc8b546e
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2025-02-07 15:30:55 +0100

    feat: update sql

--- resources/generate_group_stage_matches.sql
+++ resources/generate_group_stage_matches.sql
@@ -1,15 +1,21 @@
-CREATE OR REPLACE FUNCTION generate_group_stage_matches(p_tournament_id UUID)
+CREATE OR REPLACE FUNCTION generate_group_stage_matches(
+  p_tournament_id UUID,
+  p_start_time_ac TIME,
+  p_start_time_bd TIME
+)
   RETURNS VOID AS
 $$
 DECLARE
-  team_a           UUID;
-  team_c           UUID;
-  team_b           UUID;
-  team_d           UUID;
-  start_time       TIME := '08:00';
-  interval_minutes INT  := 15;
+  team_a        UUID;
+  team_c        UUID;
+  team_b        UUID;
+  team_d        UUID;
+  start_time_ac TIME := p_start_time_ac;
+  start_time_bd TIME := p_start_time_bd;
+  interval_ac   INT  := 15;
+  interval_bd   INT  := 15;
 BEGIN
-  -- LOOP through Groups A vs C
+  -- loop through Groups A vs C
   FOR team_a, team_c IN
     SELECT t1.id, t2.id
     FROM public.team t1
@@ -18,11 +24,11 @@ BEGIN
       AND t2.group_id IN (SELECT id FROM public.group WHERE name = 'Gruppe C' AND tournament_id = p_tournament_id)
     LOOP
       INSERT INTO public.match (tournament_id, team1_id, team2_id, start_time, round)
-      VALUES (p_tournament_id, team_a, team_c, start_time, 'Gruppenphase');
-      start_time := start_time + (interval_minutes || ' minutes')::INTERVAL;
+      VALUES (p_tournament_id, team_a, team_c, start_time_ac, 'Gruppenphase');
+      start_time_ac := start_time_ac + (interval_ac || ' minutes')::INTERVAL;
     END LOOP;
 
-  -- LOOP through Groups B vs D
+  -- loop through Groups B vs D
   FOR team_b, team_d IN
     SELECT t1.id, t2.id
     FROM public.team t1
@@ -31,8 +37,8 @@ BEGIN
       AND t2.group_id IN (SELECT id FROM public.group WHERE name = 'Gruppe D' AND tournament_id = p_tournament_id)
     LOOP
       INSERT INTO public.match (tournament_id, team1_id, team2_id, start_time, round)
-      VALUES (p_tournament_id, team_b, team_d, start_time, 'Gruppenphase');
-      start_time := start_time + (interval_minutes || ' minutes')::INTERVAL;
+      VALUES (p_tournament_id, team_b, team_d, start_time_bd, 'Gruppenphase');
+      start_time_bd := start_time_bd + (interval_bd || ' minutes')::INTERVAL;
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
