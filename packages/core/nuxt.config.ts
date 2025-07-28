5d03f468531356755464d0ab3140fa9576206a36
Dino-Kupinic <dinokupinic2006@gmail.com>
2025-07-28 16:37:29 +0200

    chore: work on deps and bundle size

--- packages/core/nuxt.config.ts
+++ packages/core/nuxt.config.ts
@@ -8,7 +8,13 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/fonts", "@nuxt/test-utils", "@nuxt/image"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/fonts",
+    "@nuxt/test-utils",
+    "@nuxt/image",
+    "@nuxtjs/supabase",
+  ],
   css: [join(currentDir, "./assets/base.css")],
   runtimeConfig: {
     public: {
