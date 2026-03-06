1ca5138cd6cf56316cb71749b4474b4f4ec78e09
Dino-Kupinic <dinokupinic2006@gmail.com>
2026-03-06 20:13:48 +0100

    feat: update media component to handle loading state and improve content display; add public read policy for misc bucket

--- apps/viewer/layouts/desktop.vue
+++ apps/viewer/layouts/desktop.vue
@@ -3,10 +3,10 @@
 <template>
   <div class="flex h-dvh w-full justify-center bg-white dark:bg-neutral-950">
     <div
-      class="flex h-dvh w-full max-w-[1440px] flex-col border-x border-neutral-200 dark:border-neutral-800"
+      class="flex h-dvh min-h-0 w-full max-w-[1440px] flex-col border-x border-neutral-200 dark:border-neutral-800"
     >
       <TheNavigation />
-      <div class="grow p-6 pb-0">
+      <div class="min-h-0 grow overflow-y-auto p-6 pb-0">
         <slot />
       </div>
     </div>
