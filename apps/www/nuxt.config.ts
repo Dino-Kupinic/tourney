71eaf1beded589031038e4adf62de2e7cabf5230
Dino-Kupinic <dinokupinic2006@gmail.com>
2026-03-10 13:17:22 +0100

    feat: small configs

--- apps/www/nuxt.config.ts
+++ apps/www/nuxt.config.ts
@@ -1,18 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ["../../packages/core"],
-
   devtools: {
     enabled: true,
   },
-
+  supabase: {
+    redirect: false,
+    types: "~~/types/database.types.ts",
+  },
   devServer: {
     port: 3002,
   },
-
   routeRules: {
     "/": { prerender: true },
   },
-
   compatibilityDate: "2025-07-28",
 })
